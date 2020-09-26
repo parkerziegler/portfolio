a79d4ee6e82ba3deecc3597e2134a7b591644d7f
parkerziegler <parker.ziegler@formidable.com>
2020-09-25 19:54:42 -0700

    Prevent header layout shift on initial render.

--- src/components/Header/SiteTitle.js
+++ src/components/Header/SiteTitle.js
@@ -6,29 +6,35 @@ const SiteTitle = () => {
   const bgRef = useRef(null);
 
   useEffect(() => {
-    if (bgRef?.current) {
-      const rc = rough.svg(bgRef);
-      bgRef.current.appendChild(
-        rc.rectangle(0, 0, 200, 55, {
-          fill: '#000000',
-          hachureAngle: 60, // angle of hachure,
-          hachureGap: 1
-        })
-      );
+    const bg = bgRef.current;
+    let roughRectangleNode = null;
+    let textNode = null;
 
-      const text = document.createElementNS(
-        'http://www.w3.org/2000/svg',
-        'text'
-      );
-      text.textContent = 'Parker Ziegler';
-      text.setAttribute('x', '8');
-      text.setAttribute('y', '36');
-      text.setAttribute(
+    if (bg) {
+      const rc = rough.svg(bgRef);
+      roughRectangleNode = rc.rectangle(0, 0, 200, 55, {
+        fill: '#000000',
+        hachureAngle: 60, // angle of hachure,
+        hachureGap: 1
+      });
+
+      bg.appendChild(roughRectangleNode);
+
+      textNode = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+      textNode.textContent = 'Parker Ziegler';
+      textNode.setAttribute('x', '8');
+      textNode.setAttribute('y', '36');
+      textNode.setAttribute(
         'style',
         "font: 3rem 'Zilla Slab', serif; fill: #ffffff"
       );
-      bgRef.current.appendChild(text);
+      bg.appendChild(textNode);
     }
+
+    return () => {
+      bg.removeChild(roughRectangleNode);
+      bg.removeChild(textNode);
+    };
   }, []);
 
   return (
@@ -40,7 +46,17 @@ const SiteTitle = () => {
           width="200"
           viewBox="0 0 200 55"
           className="fill-current text-white -ml-4"
-        ></svg>
+        >
+          <rect
+            height="55"
+            width="200"
+            x="0"
+            y="0"
+            fill="transparent"
+            aria-hidden="true"
+            className="invisible"
+          />
+        </svg>
       </a>
     </Link>
   );
