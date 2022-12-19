38a5041f92f66baedeac0d83bcbac5f84fd0e3dd
parkerziegler <parkerellisziegler@gmail.com>
2022-12-19 11:35:52 -0800

    fix: ensure Tailwind extracts styles from .tsx files during TS migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ['./src/components/**/*.js', './pages/**/*.js'],
+  content: [
+    './src/components/**/*.js',
+    './pages/**/*.js',
+    './src/components/**/*.tsx',
+    './pages/**/*.tsx'
+  ],
   theme: {
     extend: {
       colors: {
