06e70dabaedb00b3834cd47cc410d2544cae8785
parkerziegler <parkerellisziegler@gmail.com>
2022-12-18 17:01:11 -0800

    fix: rework ESLint and migrate first component to TS

---
+++ src/components/Shared/Heading.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface Props {
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}
+
+const Heading: React.FC<React.PropsWithChildren<Props>> = ({
+  tag = 'h1',
+  children,
+  ...rest
+}) => React.createElement(tag, { ...rest }, children);
+
+export default Heading;
