53a6b300261470a2465b8e8c28b913496973f9fb
parkerziegler <parkerellisziegler@gmail.com>
2024-03-11 19:47:46 -0700

    feat: Add fade effect to edges of NewsList.

--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -13,14 +13,49 @@ interface Props {
 const NewsList: React.FC<Props> = ({ className }) => {
   const [displayCount, setDisplayCount] = React.useState(3);
   const more = displayCount < NEWS.length;
+  const container = React.useRef<HTMLUListElement>(null);
 
   const onClickExpander = (showMore: boolean): void =>
     setDisplayCount((prevDisplayCount) =>
       showMore ? Math.min(prevDisplayCount + 3, NEWS.length) : 3
     );
 
+  React.useEffect(() => {
+    const node = container.current;
+
+    const handleFadeOnScroll = (n: HTMLUListElement) => {
+      return () => {
+        const isAtBottomEdge = n.scrollTop + n.clientHeight >= n.scrollHeight;
+        const isAtTopEdge = n.scrollTop === 0;
+
+        if (isAtBottomEdge) {
+          n.classList.remove('fade-edges');
+          n.classList.add('fade-edge-top');
+        } else if (isAtTopEdge) {
+          n.classList.remove('fade-edges');
+          n.classList.add('fade-edge-bottom');
+        } else {
+          n.classList.remove('fade-edge-top', 'fade-edge-bottom');
+          n.classList.add('fade-edges');
+        }
+      };
+    };
+
+    node.addEventListener('scroll', handleFadeOnScroll(node));
+
+    return () => {
+      node.removeEventListener('scroll', handleFadeOnScroll(node));
+    };
+  }, []);
+
   return (
-    <ul className={cs('text-white text-2xl font-serif', className)}>
+    <ul
+      className={cs(
+        'text-white text-2xl font-serif max-h-screen overflow-auto fade-edge-bottom',
+        className
+      )}
+      ref={container}
+    >
       {NEWS.slice(0, displayCount).map(({ id, date, description }) => (
         <li key={id} className="flex relative pb-12 last:pb-0 timeline">
           <svg
